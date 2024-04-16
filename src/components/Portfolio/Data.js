c8a017ff9564a38ae27eb6a5b95ac4a298268c14
LakshmiNarasimhan273 <bachelorofphotography@gmail.com>
2024-04-16 15:31:21 +0530

    Portfolio updated

--- src/components/Portfolio/Data.js
+++ src/components/Portfolio/Data.js
@@ -3,37 +3,43 @@ import Work1 from "../../assets/work1.jpg";
 export const portfolioData = [
   {
     id: 1,
-    image: Work1,
+    image:
+      "https://cdn.dribbble.com/users/585028/screenshots/15663487/media/8db9a367937d7c498057b31129746b64.png",
     title: "Kanban Board",
     category: "web",
   },
   {
     id: 2,
-    image: Work1,
+    image:
+      "https://cdn.dribbble.com/users/4710151/screenshots/19884157/media/b013dc6a446e89ed6923afdd00a3f6f9.png?resize=1200x900&vertical=center",
     title: "Admin Dashboard",
     category: "app",
   },
   {
     id: 3,
-    image: Work1,
-    title: "Dashboard Design",
-    category: "design",
+    image:
+      "https://cdn.dribbble.com/users/2527395/screenshots/20024553/media/b0bce0f5e3849655aecee9525b2a6274.png?resize=1200x900&vertical=center",
+    title: "Mobile App design",
+    category: "app",
   },
   {
     id: 4,
-    image: Work1,
+    image:
+      "https://cdn.dribbble.com/users/2097465/screenshots/15090818/media/aabe7850f9af090df1f0efe532b3a122.png?resize=1200x900&vertical=center",
     title: "API Development",
     category: "web",
   },
   {
     id: 5,
-    image: Work1,
+    image:
+      "https://cdn.dribbble.com/userupload/11945087/file/original-41d326b87d54e83efe1168abdcd13aeb.png?resize=1504x1128",
     title: "E-Commerce application",
     category: "design",
   },
   {
     id: 6,
-    image: Work1,
+    image:
+      "https://cdn.dribbble.com/users/2117939/screenshots/16753965/media/f696b497e1536ece8fdf054bdba0bba3.png?resize=1200x900&vertical=center",
     title: "Authentication system",
     category: "web",
   },
